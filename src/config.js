f99a6ba532618daa9b1e793c111d30fbb5b5279f
fabiangz94 <fabiangz_94@hotmail.com>
2022-11-30 01:04:24 -0600

    30 Nov

--- src/config.js
+++ src/config.js
@@ -1,9 +1,14 @@
-let PORT = 8000;
-let HOST = "localHost";
-let PROTOCOL = "http";
+let PORT = 80;
+let HOST = "turis-col-ciclo4.vercel.app";
+let PROTOCOL = "https";
 let API_ROUTE = "/api/";
 let FULL_PATH = PROTOCOL + "://" + HOST + ":" + PORT + "/";
 let FULL_API_PATH = PROTOCOL + "://" + HOST + ":" + PORT + API_ROUTE;
+
+if (PORT === 80){
+    FULL_PATH = PROTOCOL + "://" + HOST + "/";
+    FULL_API_PATH = PROTOCOL + "://" + HOST + API_ROUTE;
+}
 
 module.exports = {
     PORT,
